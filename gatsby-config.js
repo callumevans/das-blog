cf248ea0f59c54096f99cb707d44dcb6e6624950
Callum Evans <callumevans@outlook.com>
2018-08-08 03:22:21 +0100

    Final

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,11 @@
 module.exports = {
   siteMetadata: {
+    siteUrl: 'https://ssss.com',
     title: 'Gatsby Default Starter',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     'gatsby-plugin-sass',
     {
       resolve: 'gatsby-source-filesystem',
@@ -19,6 +21,9 @@ module.exports = {
         plugins: [
           {
             resolve: 'gatsby-remark-prismjs',
+            options: {
+              inlineCodeMarker: "›"
+            }
           }
         ]
       }
