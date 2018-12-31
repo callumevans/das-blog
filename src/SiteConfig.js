91dbf6623316df2b8e0885e85b36067a6e32dda8
Callum Evans <callumevans@outlook.com>
2018-12-31 18:10:30 +0000

    Update description

--- src/SiteConfig.js
+++ src/SiteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteTitle: "Callum's Blog",
   siteUrl: "https://www.callums.blog",
-  siteDescription: "Rambling about programming, software design, development practises and other related things.",
+  siteDescription: "The blog of Callum Evans. Rambling about programming, software design, development practises and other related things.",
   siteKeywords: "programming, software, callum evans, blog",
   twitterAccountId: "112250944",
   twitterCreator: "@callum_evans",
