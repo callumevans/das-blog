016ad13ead98c7d35bd00fd8a7365e8ba3051dd7
Callum Evans <callumevans@outlook.com>
2020-02-29 00:45:33 +0000

    Fallback font to avoid FOIT

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -19,7 +19,7 @@ class Layout extends React.Component {
         <Helmet
           title={SiteConfig.siteTitle}
           link={[
-            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Arvo|Scope+One' },
+            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Arvo|Scope+One&display=swap' },
             { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
             { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
             { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
