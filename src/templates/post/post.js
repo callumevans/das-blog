d08e4e9ed0ecef503d8c568590037ffebfe88f73
Callum Evans <callumevans@outlook.com>
2020-06-01 12:30:16 +0100

    Remove commento
    Remove commento

--- src/templates/post/post.js
+++ src/templates/post/post.js
@@ -3,7 +3,6 @@ import styles from './post.module.scss'
 import Layout from '../../layouts/layout'
 import PostSeo from '../../components/seo/post-seo'
 import { graphql } from 'gatsby';
-import Commento from 'react-commento';
 
 const Post = ({ data }) => (
   <Layout>
@@ -19,7 +18,6 @@ const Post = ({ data }) => (
       </header>
       <section dangerouslySetInnerHTML={{ __html: data.markdownRemark.html}} />
     </article>
-    <Commento id={data.markdownRemark.frontmatter.slug} />
   </Layout>
 )
 
